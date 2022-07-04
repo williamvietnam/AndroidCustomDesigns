f551822f102711ff0dde9102daf4872ace36d6d5
TuanNM <rik-tuannm@vtki.vn>
2022-07-04 15:52:32 +0700

    commit

--- app/src/main/java/com/material/design/components/dialog/ConfirmDialog.java
+++ app/src/main/java/com/material/design/components/dialog/ConfirmDialog.java
@@ -1,6 +1,16 @@
 package com.material.design.components.dialog;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
 
 import com.material.design.databinding.ComponentDialogConfirmBinding;
 
@@ -8,6 +18,8 @@ public class ConfirmDialog {
 
     private ComponentDialogConfirmBinding binding;
     private static ConfirmDialog instance;
+    private AlertDialog alertDialog = null;
+    private final Context context;
 
     public static ConfirmDialog getInstance(Context context) {
         if (instance == null) {
@@ -17,10 +29,102 @@ public class ConfirmDialog {
     }
 
     public ConfirmDialog(Context context) {
+        this.context = context;
+        if (context == null || (alertDialog != null && alertDialog.isShowing())) {
+            return;
+        }
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        binding = ComponentDialogConfirmBinding.inflate(inflater);
 
+        View view = binding.getRoot();
+        dialogBuilder.setView(view);
+        dialogBuilder.setCancelable(true);
+        alertDialog = dialogBuilder.create();
+        alertDialog.setCanceledOnTouchOutside(true);
+        if (alertDialog.getWindow() != null) {
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+        alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        binding.leftClick.setOnClickListener(v -> {
+            if (alertDialog != null) {
+                alertDialog.dismiss();
+            }
+        });
     }
 
-    public void show(){
+    public void show(String title, String description,
+                     String textLeftButton, String textRightButton) {
+        binding.textTitle.setText(title);
+        binding.textDescription.setText(description);
+        binding.leftClick.setText(textLeftButton);
+        binding.rightClick.setText(textRightButton);
+        if (alertDialog != null && !alertDialog.isShowing()) {
+            alertDialog.show();
+        }
+    }
+
+    public void show(@StringRes int title, @StringRes int description,
+                     @StringRes int textLeftButton, @StringRes int textRightButton) {
+        binding.textTitle.setText(context.getResources().getString(title));
+        binding.textDescription.setText(context.getResources().getString(description));
+        binding.leftClick.setText(context.getResources().getString(textLeftButton));
+        binding.rightClick.setText(context.getResources().getString(textRightButton));
+        if (alertDialog != null && !alertDialog.isShowing()) {
+            alertDialog.show();
+        }
+    }
+
+    public ConfirmDialog setCancelOnToughOutSide(boolean isCancelable) {
+        alertDialog.setCancelable(isCancelable);
+        alertDialog.setCanceledOnTouchOutside(isCancelable);
+        alertDialog.setOnKeyListener((dialog, keyCode, event) -> {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                // we cannot close dialog when we press back button
+            }
+            return false;
+        });
+
+        return this;
+    }
+
+    public ConfirmDialog setupLeftClick(@Nullable ConfirmDialog.CallBack callBack) {
+        binding.leftClick.setVisibility(View.VISIBLE);
+        binding.leftClick.setOnClickListener(v -> {
+            if (alertDialog != null) {
+                alertDialog.dismiss();
+                if (callBack != null) {
+                    callBack.onButtonLeftClick();
+                }
+            }
+        });
+
+        return this;
+    }
+
+    public ConfirmDialog setupRightClick(@Nullable ConfirmDialog.CallBack callBack) {
+        binding.rightClick.setVisibility(View.VISIBLE);
+        binding.rightClick.setOnClickListener(v -> {
+            if (alertDialog != null) {
+                alertDialog.dismiss();
+                if (callBack != null) {
+                    callBack.onButtonRightClick();
+                }
+            }
+        });
+
+        return this;
+    }
+
+    public void onDestroy() {
+        instance = null;
+    }
+
+    public interface CallBack {
+
+        void onButtonLeftClick();
+
+        void onButtonRightClick();
 
     }
 }
--- app/src/main/java/com/material/design/components/dialog/FullScreenDialog.java
+++ app/src/main/java/com/material/design/components/dialog/FullScreenDialog.java
@@ -1,6 +1,15 @@
 package com.material.design.components.dialog;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
 
 import com.material.design.databinding.ComponentDialogFullScreenBinding;
 
@@ -8,6 +17,8 @@ public class FullScreenDialog {
 
     private ComponentDialogFullScreenBinding binding;
     private static FullScreenDialog instance;
+    private AlertDialog alertDialog = null;
+    private final Context context;
 
     public static FullScreenDialog getInstance(Context context) {
         if (instance == null) {
@@ -17,6 +28,81 @@ public class FullScreenDialog {
     }
 
     public FullScreenDialog(Context context) {
+        this.context = context;
+        if (context == null || (alertDialog != null && alertDialog.isShowing())) {
+            return;
+        }
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = LayoutInflater.from(context);
+        binding = ComponentDialogFullScreenBinding.inflate(inflater);
+
+        View view = binding.getRoot();
+        dialogBuilder.setView(view);
+        dialogBuilder.setCancelable(true);
+        alertDialog = dialogBuilder.create();
+        alertDialog.setCanceledOnTouchOutside(true);
+        if (alertDialog.getWindow() != null) {
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+        alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        binding.cancelClick.setOnClickListener(v -> {
+            if (alertDialog != null) {
+                alertDialog.dismiss();
+            }
+        });
+    }
+
+    public void show(String headline) {
+        binding.textHeadline.setText(headline);
+        if (alertDialog != null && !alertDialog.isShowing()) {
+            alertDialog.show();
+        }
+    }
+
+    public void show(@StringRes int headline) {
+        binding.textHeadline.setText(context.getResources().getString(headline));
+        if (alertDialog != null && !alertDialog.isShowing()) {
+            alertDialog.show();
+        }
+    }
+
+    public FullScreenDialog setupCancelClick(@Nullable FullScreenDialog.CallBack callBack) {
+        binding.cancelClick.setVisibility(View.VISIBLE);
+        binding.cancelClick.setOnClickListener(v -> {
+            if (alertDialog != null) {
+                alertDialog.dismiss();
+                if (callBack != null) {
+                    callBack.onButtonLeftClick();
+                }
+            }
+        });
+
+        return this;
+    }
+
+    public FullScreenDialog setupSaveClick(@Nullable FullScreenDialog.CallBack callBack) {
+        binding.saveClick.setVisibility(View.VISIBLE);
+        binding.saveClick.setOnClickListener(v -> {
+            if (alertDialog != null) {
+                alertDialog.dismiss();
+                if (callBack != null) {
+                    callBack.onButtonRightClick();
+                }
+            }
+        });
+
+        return this;
+    }
+
+    public void onDestroy() {
+        instance = null;
+    }
+
+    public interface CallBack {
+
+        void onButtonLeftClick();
+
+        void onButtonRightClick();
 
     }
 }
