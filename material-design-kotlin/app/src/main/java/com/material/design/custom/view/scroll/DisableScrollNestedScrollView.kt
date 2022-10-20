4067390fe8c7deab4fd1ba25713430167c90be37
GiangNB2 <giangnb2@rikkeisoft.com>
2022-10-20 09:49:26 +0700

    kotlin: DisableScrollNestedScrollView

---
+++ material-design-kotlin/app/src/main/java/com/material/design/custom/view/scroll/DisableScrollNestedScrollView.kt
@@ -0,0 +1,22 @@
+package com.material.design.custom.view.scroll
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.core.widget.NestedScrollView
+
+class DisableScrollNestedScrollView : NestedScrollView {
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(ev: MotionEvent): Boolean {
+        return false
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+        return false
+    }
+}
