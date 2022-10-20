37a5df5ba578b0075a80441279c0abb03f249087
GiangNB2 <giangnb2@rikkeisoft.com>
2022-10-20 09:40:28 +0700

    kotlin: custom common dialog and loading dialog

---
+++ material-design-kotlin/app/src/main/java/com/material/design/custom/dialog/LoadingDialog.kt
@@ -0,0 +1,70 @@
+package com.material.design.custom.dialog
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import com.material.design.R
+import java.lang.ref.WeakReference
+
+class LoadingDialog private constructor(private val context: Context?) {
+    private var isShow = false
+    private lateinit var dialog: AlertDialog
+    fun show() {
+        if (context != null && !(context as Activity?)!!.isFinishing) {
+            if (!isShow) {
+                isShow = true
+                dialog.show()
+            }
+        }
+    }
+
+    fun hidden() {
+        if (isShow && dialog.isShowing) {
+            isShow = false
+            dialog.dismiss()
+        }
+    }
+
+    fun destroyLoadingDialog() {
+        if (instance != null) {
+            instance!!.dialog.dismiss()
+        }
+        instance = null
+    }
+
+    companion object {
+        private var instance: LoadingDialog? = null
+
+        fun getInstance(context: Context): LoadingDialog? {
+            return if (instance != null) {
+                instance
+            } else {
+                instance = LoadingDialog(
+                    WeakReference(context).get()
+                )
+                instance
+            }
+        }
+    }
+
+    init {
+        if (context != null && !isShow) {
+            val dialogBuilder =
+                AlertDialog.Builder(context)
+            val li = LayoutInflater.from(context)
+            val dialogView = li.inflate(R.layout.layout_loading, null)
+            dialogBuilder.setView(dialogView)
+            dialogBuilder.setCancelable(false)
+            dialog = dialogBuilder.create()
+            if (dialog.window != null) {
+                dialog.window!!
+                    .setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            }
+            dialog.setCancelable(false)
+            dialog.setCanceledOnTouchOutside(false)
+        }
+    }
+}
