2d708cf77ea18dfbc5d02ad0f9db19c787e4758d
GiangNB2 <giangnb2@rikkeisoft.com>
2022-10-21 11:23:02 +0700

    java: ItemHotline

---
+++ material-design-java/app/src/main/java/com/material/design/components/view/ItemHotline.java
@@ -0,0 +1,48 @@
+package com.material.design.components.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.material.design.databinding.ItemHotlineBinding;
+
+public class ItemHotline extends FrameLayout {
+    private Context context;
+
+    public ItemHotline(@NonNull Context context) {
+        super(context);
+        init(context);
+    }
+
+    public ItemHotline(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public ItemHotline(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    public ItemHotline(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private void init(final Context context) {
+        this.context = context;
+        ItemHotlineBinding binding = ItemHotlineBinding.inflate(LayoutInflater.from(context), this, true);
+        binding.llHotline.setOnClickListener(v -> {
+            Intent i = new Intent(Intent.ACTION_DIAL);
+            String p = "tel:" + "XXX";
+            i.setData(Uri.parse(p));
+            context.startActivity(i);
+        });
+    }
+}
